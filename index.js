214c91cae7a39eed89341ad56b30a20c5bfb13d7
Rachel Wyatt <rw126@evansville.edu>
2019-04-04 18:08:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function distanceFromHqInBlocks (someValue) {
 }
  
 function distanceFromHqInFeet (someValue) {
-  distanceFromHqInBlocks(someValue) * 264;
+  result = distanceFromHqInBlocks(someValue) * 264;
+  return result;
   // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
   //the return value of distanceFromHqInBlocks can then be used to calculate feet
 }
