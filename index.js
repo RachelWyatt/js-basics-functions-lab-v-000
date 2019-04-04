b699fe0aa71e2feeec72ee8a939e3253d81f09a7
Rachel Wyatt <rw126@evansville.edu>
2019-04-04 17:59:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (someValue) {
   //returns the number of blocks given a value
+  return = 42 - 
 }
  
 function distanceFromHqInFeet (someValue) {
